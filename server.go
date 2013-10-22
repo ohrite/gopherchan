b1b65580f1d5d0c8727babe180029a26f5f90758
Doc Ritezel <pair+doc@ministryofvelocity.com>
2013-10-21 20:13:00 -0700

    Add request logging

--- server.go
+++ server.go
@@ -3,7 +3,9 @@ package main
 import (
   "os"
   "log"
+  "net"
 
+  "github.com/ohrite/gopher"
   "github.com/ohrite/gopherchan/gopherchan"
   "github.com/ohrite/gopherchan/controllers"
 )
@@ -11,12 +13,15 @@ import (
 func main() {
   port := os.Getenv("PORT")
   router := gopherchan.NewRouter()
-  server := gopherchan.NewServer(port, router.HandleRequest)
-  log.Printf("Serving at %v", server.URL())
+  server := gopherchan.NewServer(port, func(connection net.Conn, request *gopher.Request) {
+    log.Printf("Handling request: %v", request.URL.String())
+    router.HandleRequest(connection, request)
+  })
   router.AddRoute("/new", &controllers.PostCreateController{Host: "localhost", Port: server.Port})
   router.AddRoute("/:id", &controllers.PostGetController{Host: "localhost", Port: server.Port})
   router.AddRoute("/", &controllers.PostIndexController{Host: "localhost", Port: server.Port})
   router.AddRoute("", &controllers.PostIndexController{Host: "localhost", Port: server.Port})
   router.AddRoute("*", &controllers.ErrorController{Host: "localhost", Port: server.Port})
+  log.Printf("Serving at %v", server.URL())
   log.Fatal(server.Serve())
 }
